2e5e3f184e56479c68a114a538e2b430244d7f07
DESKTOP-56IP0E3\whwlg <whwlgns42@kakao.com>
2025-03-13 13:30:20 +0900

    Updates

---
+++ assets/LoadingButton-Dsc-v8oU.js
@@ -0,0 +1,2 @@
+import{r,j as e,B as a}from"./index-C94LjWxh.js";let t=!1;const i=()=>{t||(console.warn(["MUI: The LoadingButton component functionality is now part of the Button component from Material UI.","","You should use `import Button from '@mui/material/Button'`","or `import { Button } from '@mui/material'`"].join(`
+`)),t=!0)},m=r.forwardRef(function(o,n){return i(),e.jsx(a,{ref:n,...o})});export{m as L};
---
+++ assets/Person-0bM3299F.js
@@ -0,0 +1,1 @@
+import{c as s,j as o}from"./index-C94LjWxh.js";const r=s(o.jsx("path",{d:"M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4m0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4"}),"Person");export{r as P};
